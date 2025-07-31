45d1a50cee93d485971e361eaa90acfa2e8d76b7
bohee111 <bboohhee06@gmail.com>
2025-07-31 14:20:22 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@
 API_KEY = "YOUR_ACTUAL_GEMINI_API_KEY"
 
 # 공통 파일명
-NEWS_CSV_FILENAME = "monthly_news.csv"
-IMPACT_SCORE_CSV_FILENAME = "news_with_impact_score.csv"
+NEWS_CSV_FILENAME = "data/monthly_news.csv"
+IMPACT_SCORE_CSV_FILENAME = "data/news_with_impact_score.csv"
 EXCHANGE_RATE_CSV = "data/주요국 통화의 대원화환율_30133713.csv"
